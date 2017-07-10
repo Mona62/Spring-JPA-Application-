158e301b337dee335b86692a3c4223707e3ceada
Mona <mona.annaparthi62@gmail.com>
2017-07-09 22:32:22 -0400

    inserted a new constant

--- src/net/antra/deotemp/utility/Constants.java
+++ src/net/antra/deotemp/utility/Constants.java
@@ -3,4 +3,5 @@ package net.antra.deotemp.utility;
 public class Constants{
 	public static final int DEFAUL_DEPT_STATUS_ID = 1;
 	public static final int DEFAUL_EMP_STATUS_ID = 1;
+	public static final int DEFAUL_CONSTANT = 2;
 }
